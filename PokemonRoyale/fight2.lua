8ad245667317a682dcbdb57e12ca2b9a41900a5a
Dburris13 <dab0021@uah.edu>
2016-11-23 16:54:05 -0600

    Adding buttons and stub functions to fight scene

--- PokemonRoyale/fight2.lua
+++ PokemonRoyale/fight2.lua
@@ -8,7 +8,7 @@
 local composer = require("composer")
 local scene = composer.newScene()
 local widget = require("widget")
-local Pokemon = require ("Pokemon")
+local Pokemon = require ("Pokemon");
 
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
@@ -18,81 +18,27 @@ local Pokemon = require ("Pokemon")
 local menu
 local platformBG
 local cancelBtn
-local mainMenuBtn = {}
 local fightMenuBtn = {}
-local pkmnMenuBtn = {}
-local pokemon = Pokemon:new( {HP=150} )
-local sceneGroup
+local mainMenuBtn = {}
+local pokemon = Pokemon:new( {HP=150} );
 
 -- Local Sounds
 local menuClick = audio.loadStream("sounds/menuButtonClick.mp3")
 
--- Fight Menu Functions
-function attack1(event)
-    if ( "ended" == event.phase ) then
-        print("attack 1")
-    end
+function attack1()
+    print("attack 1")
 end
 
-function attack2(event)
-    if ( "ended" == event.phase ) then
-        print("attack 2")
-    end
+function attack2()
+    print("attack 2")
 end
 
-function attack3(event)
-    if ( "ended" == event.phase ) then
-        print("attack 3")
-    end
+function attack3()
+    print("attack 3")
 end
 
-function attack4(event)
-    if ( "ended" == event.phase ) then
-        print("attack 4")
-    end
-end
-
--- Pokemon Menu Functions
-function select1(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 1")
-    end
-end
-
-function select2(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 2")
-    end
-end
-
-function select3(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 3")
-    end
-end
-
-function select4(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 4")
-    end
-end
-
-function select5(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 5")
-    end
-end
-
-function select6(event)
-    if ( "ended" == event.phase ) then
-        print("select pokemon 6")
-    end
-end
-
-function exitButtonEvent(event)
-    if ("ended" == event.phase) then
-        composer.gotoScene("menu")
-    end
+function attack4()
+    print("attack 4")
 end
 
 function drawBackground()
@@ -102,15 +48,14 @@ function drawBackground()
     platformBG.x = display.pixelWidth - (display.pixelWidth/2)
     platformBG.y = display.pixelHeight - (display.pixelHeight/1.33) 
 
-    local pokemon1 = pokemon:new({xPos=542, yPos=380})
+    local pokemon1 = pokemon:new({xPos=542, yPos=340})
     pokemon1:create(3)
     pokemon1:setSelectionView()
 
-    local pokemon2 = pokemon:new({xPos=190, yPos=530})
+    local pokemon2 = pokemon:new({xPos=190, yPos=560})
     pokemon2:create(6)
     pokemon2:setBattleView()
 
-    sceneGroup:insert( platformBG )
 end
 
 function returnToMainMenu(event)
@@ -118,20 +63,17 @@ function returnToMainMenu(event)
         audio.play(menuClick, {loops = 0})
         if (fightMenuBG) then
             fightMenuBG.isVisible = false
-            for cnt = 0, #fightMenuBtn do
+            for cnt = 0, 3 do
                 fightMenuBtn[cnt].isVisible = false
             end
         end
 
-        if (pkmnMenuBG) then
-            pkmnMenuBG.isVisible = false
-            for cnt = 0, #pkmnMenuBtn do
-                pkmnMenuBtn[cnt].isVisible = false
-            end
+        if (partyScreen) then
+            partyScreen.isVisible = false
         end
 
-        if (itemsMenuBG) then
-            itemsMenuBG.isVisible = false
+        if (itemsMenu) then
+            itemsMenu.isVisible = false
             for cnt = 0, #itemList do
                 itemList[cnt].isVisible = false
             end
@@ -139,7 +81,7 @@ function returnToMainMenu(event)
 
         cancelBtn.isVisible = false
 
-        mainMenuBG.isVisible = true
+        menu.isVisible = true
         for cnt = 0, 3 do
             mainMenuBtn[cnt].isVisible = true
         end
@@ -147,9 +89,9 @@ function returnToMainMenu(event)
 end
 
 function openMainMenu ()
-    mainMenuBG = display.newImage("images/fightScene/menu/main/mainMenuBG.png")
-    mainMenuBG.width = display.pixelWidth
-    mainMenuBG.height = display.pixelHeight/2
+    menu = display.newImage("images/fightScene/menu/main/mainMenuBG.png")
+    menu.width = display.pixelWidth
+    menu.height = display.pixelHeight/2
 
     mainMenuBtn[0] = widget.newButton({    
             id = "fightBtn",
@@ -203,19 +145,13 @@ function openMainMenu ()
             fontSize = 40,
             defaultFile = "images/fightScene/menu/main/RunButton.png",
             overFile  = "images/fightScene/menu/main/RunButtonOnClick.png",
-            onEvent = exitButtonEvent 
+            onEvent = openRunMenu 
         } )
     mainMenuBtn[3].x = 475
     mainMenuBtn[3].y = 1105    
 
-    mainMenuBG.x = display.pixelWidth - (display.pixelWidth/2)
-    mainMenuBG.y = display.pixelHeight - (display.pixelHeight/4) 
-
-    sceneGroup:insert( mainMenuBG )
-    sceneGroup:insert( mainMenuBtn[0] )
-    sceneGroup:insert( mainMenuBtn[1] )
-    sceneGroup:insert( mainMenuBtn[2] )
-    sceneGroup:insert( mainMenuBtn[3] )
+    menu.x = display.pixelWidth - (display.pixelWidth/2)
+    menu.y = display.pixelHeight - (display.pixelHeight/4) 
 end
 
 function openFightMenu (event)
@@ -228,47 +164,27 @@ function openFightMenu (event)
         fightMenuBG.x = display.pixelWidth - (display.pixelWidth/2)
         fightMenuBG.y = display.pixelHeight - (display.pixelHeight/4) 
 
-        fightMenuBtn[0] = widget.newButton({    
-            id = "attack1Btn",
-            width = 333,
-            height = 206,
-            defaultFile = "images/fightScene/menu/fight/fightMenuBtn.png",
-            overFile  = "images/fightScene/menu/fight/fightMenuBtnOnClick.png",
-            onEvent = attack1 
-        } )        
+        fightMenuBtn[0] = display.newImage("images/fightScene/menu/fight/fightMenuBtn.png")
+        fightMenuBtn[0].width = 333
+        fightMenuBtn[0].height = 206 
         fightMenuBtn[0].x = 184
-        fightMenuBtn[0].y = 842        
-
-        fightMenuBtn[1] = widget.newButton({    
-            id = "attack2Btn",
-            width = 333,
-            height = 206,
-            defaultFile = "images/fightScene/menu/fight/fightMenuBtn.png",
-            overFile  = "images/fightScene/menu/fight/fightMenuBtnOnClick.png",
-            onEvent = attack2 
-        } )        
+        fightMenuBtn[0].y = 842
+        
+        fightMenuBtn[1] = display.newImage("images/fightScene/menu/fight/fightMenuBtn.png")
+        fightMenuBtn[1].width = 333
+        fightMenuBtn[1].height = 206   
         fightMenuBtn[1].x = 536
         fightMenuBtn[1].y = 842
-
-        fightMenuBtn[2] = widget.newButton({    
-            id = "attack3Btn",
-            width = 333,
-            height = 206,
-            defaultFile = "images/fightScene/menu/fight/fightMenuBtn.png",
-            overFile  = "images/fightScene/menu/fight/fightMenuBtnOnClick.png",
-            onEvent = attack3 
-        } )        
+        
+        fightMenuBtn[2] = display.newImage("images/fightScene/menu/fight/fightMenuBtn.png")
+        fightMenuBtn[2].width = 333
+        fightMenuBtn[2].height = 206 
         fightMenuBtn[2].x = 184
         fightMenuBtn[2].y = 1074    
 
-        fightMenuBtn[3] = widget.newButton({    
-            id = "attack4Btn",
-            width = 333,
-            height = 206,
-            defaultFile = "images/fightScene/menu/fight/fightMenuBtn.png",
-            overFile  = "images/fightScene/menu/fight/fightMenuBtnOnClick.png",
-            onEvent = attack4 
-        } )      
+        fightMenuBtn[3] = display.newImage("images/fightScene/menu/fight/fightMenuBtn.png")
+        fightMenuBtn[3].width = 333
+        fightMenuBtn[3].height = 206 
         fightMenuBtn[3].x = 536
         fightMenuBtn[3].y = 1074        
 
@@ -283,99 +199,26 @@ function openFightMenu (event)
         cancelBtn.x = 638
         cancelBtn.y = 1226    
 
-        --fightMenuBtn[0]:addEventListener("tap", attack1);
-        fightMenuBtn[1]:addEventListener("tap", attack2)
-        fightMenuBtn[2]:addEventListener("tap", attack3)
-        fightMenuBtn[3]:addEventListener("tap", attack4)
+        fightMenuBtn[0]:addEventListener("tap", attack1);
+        fightMenuBtn[1]:addEventListener("tap", attack2);
+        fightMenuBtn[2]:addEventListener("tap", attack3);
+        fightMenuBtn[3]:addEventListener("tap", attack4);
 
-        mainMenuBG.isVisible = false
+        menu.isVisible = false
         for cnt = 0, 3 do
             mainMenuBtn[cnt].isVisible = false
         end
-
-        sceneGroup:insert( fightMenuBG )
-        sceneGroup:insert( fightMenuBtn[0] )
-        sceneGroup:insert( fightMenuBtn[1] )
-        sceneGroup:insert( fightMenuBtn[2] )
-        sceneGroup:insert( fightMenuBtn[3] )
-        sceneGroup:insert( cancelBtn )
     end
 end
 
 function openPokemonMenu(event)
     if ( "ended" == event.phase ) then
         audio.play(menuClick, {loops = 0})
-        pkmnMenuBG = display.newImage("images/fightScene/menu/pkmn/pkmnMenuBG.png")
-        pkmnMenuBG.width = display.pixelWidth
-        pkmnMenuBG.height = display.pixelHeight/2 
-        pkmnMenuBG.x = display.pixelWidth - (display.pixelWidth/2)
-        pkmnMenuBG.y = display.pixelHeight - (display.pixelHeight/4)    
-
-        pkmnMenuBtn[0] = widget.newButton({    
-            id = "select1Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select1 
-        } )        
-        pkmnMenuBtn[0].x = 180
-        pkmnMenuBtn[0].y = 716  
-
-        pkmnMenuBtn[1] = widget.newButton({    
-            id = "select2Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select2 
-        } )        
-        pkmnMenuBtn[1].x = 540
-        pkmnMenuBtn[1].y = 752 
-
-        pkmnMenuBtn[2] = widget.newButton({    
-            id = "select3Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select3 
-        } )        
-        pkmnMenuBtn[2].x = 180
-        pkmnMenuBtn[2].y = 887 
-
-        pkmnMenuBtn[3] = widget.newButton({    
-            id = "select4Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select4 
-        } )        
-        pkmnMenuBtn[3].x = 540
-        pkmnMenuBtn[3].y = 914 
-
-        pkmnMenuBtn[4] = widget.newButton({    
-            id = "select5Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select5 
-        } )        
-        pkmnMenuBtn[4].x = 180
-        pkmnMenuBtn[4].y = 1047 
-
-        pkmnMenuBtn[5] = widget.newButton({    
-            id = "select6Btn",
-            width = 355,
-            height = 150,
-            defaultFile = "images/fightScene/menu/pkmn/pkmnMenuBtn.png",
-            overFile  = "images/fightScene/menu/pkmn/pkmnMenuBtnOnClick.png",
-            onEvent = select6 
-        } )        
-        pkmnMenuBtn[5].x = 540
-        pkmnMenuBtn[5].y = 1074 
+        partyScreen = display.newImage("images/fightScene/menu/pkmn/pkmnMenuBG.png")
+        partyScreen.width = display.pixelWidth
+        partyScreen.height = display.pixelHeight/2 
+        partyScreen.x = display.pixelWidth - (display.pixelWidth/2)
+        partyScreen.y = display.pixelHeight - (display.pixelHeight/4)    
 
         cancelBtn = widget.newButton({    
                 id = "cancelBtn",
@@ -388,30 +231,21 @@ function openPokemonMenu(event)
         cancelBtn.x = 638
         cancelBtn.y = 1226  
 
-        mainMenuBG.isVisible = false
+        menu.isVisible = false
         for cnt = 0, 3 do
             mainMenuBtn[cnt].isVisible = false
         end
-
-        sceneGroup:insert( pkmnMenuBG )
-        sceneGroup:insert( pkmnMenuBtn[0] )
-        sceneGroup:insert( pkmnMenuBtn[1] )
-        sceneGroup:insert( pkmnMenuBtn[2] )
-        sceneGroup:insert( pkmnMenuBtn[3] )        
-        sceneGroup:insert( pkmnMenuBtn[4] ) 
-        sceneGroup:insert( pkmnMenuBtn[5] ) 
-        sceneGroup:insert( cancelBtn )
     end
 end
 
 function openItemsMenu (event)
     if ( "ended" == event.phase ) then
         audio.play(menuClick, {loops = 0})
-        itemsMenuBG = display.newImage("images/fightScene/menu/item/itemMenuBG.png")
-        itemsMenuBG.width = display.pixelWidth
-        itemsMenuBG.height = display.pixelHeight/2
-        itemsMenuBG.x = display.pixelWidth - (display.pixelWidth/2)
-        itemsMenuBG.y = display.pixelHeight - (display.pixelHeight/4)    
+        itemsMenu = display.newImage("images/fightScene/menu/item/itemMenuBG.png")
+        itemsMenu.width = display.pixelWidth
+        itemsMenu.height = display.pixelHeight/2
+        itemsMenu.x = display.pixelWidth - (display.pixelWidth/2)
+        itemsMenu.y = display.pixelHeight - (display.pixelHeight/4)    
 
 
         itemList = {}
@@ -431,15 +265,10 @@ function openItemsMenu (event)
         cancelBtn.x = 638
         cancelBtn.y = 1226  
 
-        mainMenuBG.isVisible = false
+        menu.isVisible = false
         for cnt = 0, 3 do
             mainMenuBtn[cnt].isVisible = false
-        end   
-
-        sceneGroup:insert( itemsMenuBG )
-        sceneGroup:insert( itemList[0] )
-        sceneGroup:insert( itemList[1] )        
-        sceneGroup:insert( cancelBtn ) 
+        end    
     end
 end
 
@@ -457,12 +286,10 @@ function openRunMenu (event)
         cancelBtn.x = 638
         cancelBtn.y = 1226  
 
-        mainMenuBG.isVisible = false
+        menu.isVisible = false
         for cnt = 0, 3 do
             mainMenuBtn[cnt].isVisible = false
-        end      
-
-        sceneGroup:insert( cancelBtn ) 
+        end    
     end
 end
 -- -----------------------------------------------------------------------------------
